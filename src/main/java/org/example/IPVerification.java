8e8dd8129d7809b39cd2e44b15714044ee04ee35
Kovalenko <amidalo148@gmail.com>
2025-01-29 01:40:03 +0700

    Add a null check and use UnknownHostException instead of Exception

--- src/main/java/org/example/IPVerification.java
+++ src/main/java/org/example/IPVerification.java
@@ -1,13 +1,18 @@
 package org.example;
 
 import java.net.Inet4Address;
+import java.net.UnknownHostException;
 
 public class IPVerification {
-    public boolean isValidIP(String ip) {
+    public boolean isValidIPv4(String ip) {
+        if (ip == null || ip.isEmpty()) {
+            return false;
+        }
+
         try {
             Inet4Address addr = (Inet4Address) Inet4Address.getByName(ip);
             return addr.getHostAddress().equals(ip);
-        } catch (Exception e) {
+        } catch (UnknownHostException e) {
             return false;
         }
     }
